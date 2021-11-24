2b5f6f1fac82532e1c4a0c1bf132579bb91edddb
danorel <mail.ordan@gmail.com>
2021-11-24 20:26:28 +0200

    Add Matrix data type

---
+++ lib/matrix.h
@@ -0,0 +1,10 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+template <typename T>
+struct Matrix {
+	size_t length;
+	T* data;
+};
+
+#endif
--- src/classic.cpp
+++ src/classic.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 #include </usr/local/include/mpi.h>
+#include "../lib/matrix.h"
 
 int main(int argc, char** argv) {
+	Matrix<int> m;
+
+	size_t length = 5;
+	int *data = new int[length];
+	
+	for (int i = 0; i < length; ++i) {
+		*(data + i) = i + 1;
+	}
+
+	m.data = data;
+	m.length = length;
+
+	for (int i = 0; i < m.length; ++i) {
+		std::cout << *(m.data + i) << " ";
+	}
+
 	return 0;
 }
